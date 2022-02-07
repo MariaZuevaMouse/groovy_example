8a6cdb239dd98f71894edfd2a55e931187381d20
Mariia Zueva <58681283+MariaZuevaMouse@users.noreply.github.com>
2022-02-07 09:58:46 +0300

    initial files

---
+++ jenkinsfile.groovy
@@ -0,0 +1,34 @@
+//import static com.github.wololock.groovy.Dsl.pipeline
+
+println '''
+   ___                                ___ _            _ _            
+  / _ \\_ __ ___   _____   ___   _    / _ (_)_ __   ___| (_)_ __   ___ 
+ / /_\\/ '__/ _ \\ / _ \\ \\ / / | | |  / /_)/ | '_ \\ / _ \\ | | '_ \\ / _ \\
+/ /_\\\\| | | (_) | (_) \\ V /| |_| | / ___/| | |_) |  __/ | | | | |  __/
+\\____/|_|  \\___/ \\___/ \\_/  \\__, | \\/    |_| .__/ \\___|_|_|_| |_|\\___|
+                            |___/          |_|                        
+'''
+
+pipeline {
+    agent any
+
+    environment {
+        SOME_NUMBER = 123
+        SOME_STRING = "foobar"
+    }
+
+    stages{
+        stage("Build"){
+            steps{
+                sh "ls -la"
+                sh(script: 'date +%Y-%m-%d', returnStdout: false)
+                echo "Groovy rocks!"
+            }
+        }
+        stage("Test"){
+            steps{
+                sh "mvn -version"
+            }
+        }
+    }
+}
---
+++ src/com/dsl_example/Dsl.groovy
@@ -0,0 +1,4 @@
+package com.dsl_example
+
+class Dsl {
+}
