24010fb12c630167244bee5d1630ee69a0638152
Mariia Zueva <58681283+MariaZuevaMouse@users.noreply.github.com>
2022-02-07 10:29:23 +0300

    add PipelineDsl class and delegate method from jenkinsfile to this class

--- com/dsl_example/Dsl.groovy
+++ com/dsl_example/Dsl.groovy
@@ -2,6 +2,23 @@ package com.dsl_example
 
 class Dsl {
     static void pipeline(final Closure closure) {
+        final PipelineDsl dsl = new PipelineDsl()
+        closure.delegate = dsl
+        closure.resolveStrategy = Closure.DELEGATE_ONLY
+        closure.call()
+    }
+}
+
+class PipelineDsl {
+    void agent() {
+
+    }
+
+    void environment(final Closure closure) {
+
+    }
+
+    void stages(final Closure closure) {
 
     }
 }
